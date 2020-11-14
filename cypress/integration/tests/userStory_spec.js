d4c07a788df0d085d78808ea23d858d4e1b32f8c
Vish Sharma <vish.sharma@grabone.conz>
2020-11-15 00:43:32 +1300

    Added mutiple viewport support

--- cypress/integration/tests/userStory_spec.js
+++ cypress/integration/tests/userStory_spec.js
@@ -1,22 +1,28 @@
 import infoText from "../testData/infoMsgs";
 import formData from "../testData/formTestData";
+import screenSize from "../testData/testconfig";
 
 const { navigateToKsCalculator, validateInfoMsg, fillForm } = require("../utils/ksCalculator");
 
 describe("Run Calculator Test Scenarios ", () => {
   beforeEach("Load the page before the entire tests runs", () => {
     cy.visit("/");
-    navigateToKsCalculator();
   });
 
-  it("TC01 : Check Info icon present on all the fields in Ks Ret Calculator", () => {
-    validateInfoMsg(infoText);
-  });
+  screenSize.forEach(size => {
+    it(`TC01 : Check Info icon present on all the fields in Ks Ret Calculator -- ${size}`, () => {
+      cy.viewport(size);
+      navigateToKsCalculator();
+      validateInfoMsg(infoText);
+    });
 
-  it("TC02 : Users should able to calculate their projected KiwiSaver balance", () => {
-    cy.wrap(formData).each(data => {
-      fillForm(data);
-      cy.reload();
+    it(`TC02 : Users should able to calculate their projected KiwiSaver balance -- ${size}`, () => {
+      cy.viewport(size);
+      navigateToKsCalculator();
+      formData.forEach(data => {    
+        fillForm(data);
+        cy.reload();
+      });
     });
   });
 });
